cf365aa42a0794d7aa214662a691ed28530cbf88
Pedro Rico <pedro.rico@fastcode.today>
2026-02-25 21:55:50 -0600

    Commit de prueba

---
+++ src/main/java/com/proyecto1/inndata20/entity/PersonaEntity.java
@@ -0,0 +1,10 @@
+package com.proyecto1.inndata20.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+
+public class PersonaEntity {
+}
